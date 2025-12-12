3ac99cf0b630c509e5688e0c9d57b2e9b23f77e2
Hitesh <mehiteshmondal@gmail.com>
2025-12-12 12:14:32 +0530

    modified:   devops-webapp/app.js

--- devops-webapp/app.js
+++ devops-webapp/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const promClient = require('prom-client');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Prometheus metrics
 const register = new promClient.Registry();
